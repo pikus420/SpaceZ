3646f6ebff5eac1f74ff0a75f35be6e3757385cd
pikus420 <67975041+pikus420@users.noreply.github.com>
2022-01-17 16:25:38 +0100

    dodano burger menu

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const burger = document.querySelector("#burger")
+const menu = document.querySelector("nav")
 let flag = false
 burger.addEventListener("click", ()=>{
     if(!flag)
     {
         burger.classList.add("opened")
+        menu.style.display = "block"
         flag = true;
     }
     else
     {
         burger.classList.remove("opened")
+        menu.style.display = "none"
         flag = false;
     }
 
