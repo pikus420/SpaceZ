b0b1e8bb0ed82608b017554b280fe5be27997878
pikus420 <67975041+pikus420@users.noreply.github.com>
2022-01-18 18:24:45 +0100

    dodano funkcjonalność galerii

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const burger = document.querySelector("#burger")
 const menu = document.querySelector("nav")
+const obrazy = document.querySelectorAll(".obraz")
+
+
 let flag = false
 burger.addEventListener("click", ()=>{
     if(!flag)
@@ -25,5 +28,25 @@ window.addEventListener("resize", ()=>{
         burger.classList.remove("opened")
         flag = false;
     }
-    
 })
+
+
+function viewImage(path)
+{
+    const obraz = document.createElement("img")
+    obraz.src = path
+    obraz.classList.add("zoom");
+    obraz.addEventListener("click", function ()
+    {
+        this.remove()
+        document.body.style.overflow = "visible"
+    })
+    document.body.appendChild(obraz)
+    document.body.style.overflow = "hidden"
+}
+
+obrazy[0].addEventListener("click", ()=>viewImage("img/obraz1.jpg"))
+obrazy[1].addEventListener("click", ()=>viewImage("img/obraz2.jpg"))
+obrazy[2].addEventListener("click", ()=>viewImage("img/obraz3.jpg"))
+obrazy[3].addEventListener("click", ()=>viewImage("img/obraz4.jpg"))
+obrazy[4].addEventListener("click", ()=>viewImage("img/obraz5.jpg"))
